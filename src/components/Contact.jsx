fae9f286af832fc277e3e0c43c7d0671fa74990b
Ashwin P S <ashwinsiva54@gmail.com>
2021-09-26 11:39:46 +0530

    completed project files

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
-
+import fb from './images/fb.png';
+import github from './images/github.png';
+import instagram from './images/instagram.png';
+import linkedin from './images/linkedin.png';
 function Contact() {
     return (
-        <div>
-            <h1>ContactPage</h1>
+        <div className="contact">
+        <div className="contactContainer">
+        <span className="material-icons">email</span>&nbsp;
+        <a href="mailto:ashwinsiva54@gmail.com"> Gmail: ashwinsiva54 </a>
+        </div>
+        <div className="socialContainer">
+            <a href="https://ashwin-p-s.github.io/socialmedia" target="_blank" rel="noreferrer"> Stay Connected to My Socials  </a>
+            <div className="imageContainer">
+            <img src={ linkedin } alt="linkedin" />
+            <img src={ github } alt="github" />
+            <img src={ instagram } alt="instagram" />
+            <img src={ fb } alt="fb" />
+            </div>
+        </div>
         </div>
     )
 }
