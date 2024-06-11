0388ffe85cf10db694a23d8d151dbb88fb09463c
Ashwin-P-S <ashwinsiva54@gmail.com>
2024-06-11 12:43:11 +0530

    update: removed additional pages in UI Layout and improved UX

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
 import React from "react";
-import NavBar from "./components/NavBar";
+import {useState} from "react";
 import Home from "./components/Home";
-import Projects from "./components/Projects";
 import About from "./components/About";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import NavBar from "./components/NavBar";
+import Projects from "./components/Projects";
 import AOS from "aos";
 
 function App() {
-  AOS.init();
+    AOS.init();
+    var count = 500;
+    const [currentPage, setCurrentPage] = useState(1);
 
-  var count = 500;
-
-  return (
-    <Router>
-      <div className="card">
-        <NavBar />
-        <Route path="/" exact component={Home} />
-        <Route path="/projects" exact component={Projects} />
-        <Route path="/about" exact render={() => <About count={count} />} />
-      </div>
-    </Router>
-  );
+    return (
+        <main className="card">
+            <NavBar setCurrentPage={setCurrentPage}/>
+            {(currentPage === 1) && <Home />}
+            {(currentPage === 2) && <Projects />}
+            {(currentPage === 3) && <About count={count} />}
+        </main>
+    );
 }
 
 export default App;
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,55 +1,67 @@
 import React from 'react';
-import { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
 import '../index.css';
+import {useState, useEffect} from 'react';
 
-function NavBar() {
+function NavBar({setCurrentPage}) {
 
-  const toggleNav = () => {
-    setToggleMenu(!toggleMenu);
-  }
-
-  useEffect(() => {
-
-    const changeWidth = () => {
-      setScreenWidth(window.innerWidth);
+    const toggleNav = () => {
+        setToggleMenu(!toggleMenu);
     }
 
-    window.addEventListener('resize', changeWidth);
+    const changeCurrentPage = (n) => {
+        setCurrentPage(n);
+    }
 
-  }, [])
+    useEffect(() => {
 
-  const [screenWidth, setScreenWidth] = useState(window.innerWidth);
-  const [toggleMenu, setToggleMenu] = useState(false);
+        const changeWidth = () => {
+            setScreenWidth(window.innerWidth);
+        }
 
-  return (
+        window.addEventListener('resize', changeWidth);
 
-    <div className="navbar" >
+    }, [])
 
-      <div className="menuBtn">
-        <div className="menu" onClick={toggleNav} >
-          <div className="bar1"></div>
-          <div className="bar2"></div>
-          <div className="bar3"></div>
+    const [screenWidth, setScreenWidth] = useState(window.innerWidth);
+    const [toggleMenu, setToggleMenu] = useState(false);
+
+    return (
+
+        <div className="navbar">
+
+            <div className="menuBtn">
+                <div className="menu" onClick={toggleNav}>
+                    <div className="bar1"></div>
+                    <div className="bar2"></div>
+                    <div className="bar3"></div>
+                </div>
+            </div>
+
+            <div className="welcome">
+                <p> Hi, Nice to Meet You! </p>
+            </div>
+
+            {
+                (toggleMenu || screenWidth > 600) && (
+                    <div className="linksContainer">
+                        <div className="links" onClick={() => {
+                            toggleNav();
+                            changeCurrentPage(1);
+                        }}> Home <span></span></div>
+                        <div className="links" onClick={() => {
+                            toggleNav();
+                            changeCurrentPage(2);
+                        }}> Projects <span></span></div>
+                        <div className="links" onClick={() => {
+                            toggleNav();
+                            changeCurrentPage(3);
+                        }}> About <span></span></div>
+                    </div>
+                )
+            }
+
         </div>
-      </div>
-
-      <div className="welcome">
-        <p> Hi, Nice to Meet You! </p>
-      </div>
-
-      {
-        (toggleMenu || screenWidth > 600) && (
-          <div className="linksContainer">
-            <Link to='/' className="links" onClick={toggleNav} data-aos={"fade-down"} > Home <span></span></Link>
-            <Link to='/projects' className='links' onClick={toggleNav} data-aos={"fade-down"}> Projects <span></span> </Link>
-            <Link to='/about' className="links" onClick={toggleNav} data-aos={"fade-down"}> About <span></span> </Link>
-          </div>
-        )
-      }
-
-    </div>
-  )
+    )
 }
 
 export default NavBar
